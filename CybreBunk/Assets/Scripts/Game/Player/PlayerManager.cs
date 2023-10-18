14750fc20f2b3d6adc3b3879d2591ade6134f0e8
Edmalm <nilswilliam2005@gmail.com>
2023-10-18 10:54:10 +0200

    hjk

--- CybreBunk/Assets/Scripts/Game/Player/PlayerManager.cs
+++ CybreBunk/Assets/Scripts/Game/Player/PlayerManager.cs
@@ -12,3 +12,20 @@ static class PlayerManager
     //is currently in DamageDealer
     public static TarotData FindTarotCard() => selectedCard ? selectedCard : ScriptableObject.CreateInstance<TarotData>();
 }
+
+static class ItemManager
+{
+    [SerializeField] static ItemData[] items;
+    static ItemData[] Items
+    {
+        get => GetItems();
+        set => throw new System.NotImplementedException();
+    }
+    static ItemData[] GetItems()
+    {
+        return null;
+        //FIX THIS
+        //https://discussions.unity.com/t/how-can-i-find-all-instances-of-a-scriptable-object-in-the-project-editor/198002/3
+    }
+        
+}
---
+++ CybreBunk/Assets/Scripts/Game/Ui/UserInterfaceItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UserInterfaceItem : MonoBehaviour
+{
+    [SerializeField] TMP_Text[] description;
+    void OnPickUp()
+    {
+        
+    }
+    
+    void PickRandomItem()
+    {
+        
+    }
+}
