2f935eb5bd803ac1aba87434f4c7b4c54a8d3183
Edmalm <nilswilliam2005@gmail.com>
2023-10-25 15:16:34 +0200

    rider flow crabrave

--- CybreBunk/Assets/Scripts/Game/Management/PauseMenu.cs
+++ CybreBunk/Assets/Scripts/Game/Management/PauseMenu.cs
@@ -6,21 +6,36 @@ using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] Canvas     canvas;
+    [SerializeField] Canvas     mainCanvas;
     [SerializeField] GameObject currentTarot;
+    
+    [Header("Settings")]
+    [SerializeField] Canvas settingsCanvas;
+    //[SerializeField] Slider distortionSlider;
+    bool                    inSettingsMenu = false;
+    
     float                       timeScaleAtStart = 1;
     bool                        isPaused;
     void Start()
     {
         DiosBestFriend(false);
-
+        settingsCanvas.enabled                    = false;
+        
         currentTarot.GetComponent<Image>().sprite = PlayerManager.selectedCard?.CurrentCard;
+        
     }
     public void DiosBestFriend(bool freeze)
     {
+        if (inSettingsMenu)
+        {
+            SettingsToggle(false);
+            return;
+        }
+
+        ;
         IsPaused       = freeze;
         Time.timeScale = freeze ? 0 : timeScaleAtStart;
-        canvas.enabled = freeze;
+        mainCanvas.enabled = freeze;
     }
     public bool IsPaused
     {
@@ -32,5 +47,18 @@ public class PauseMenu : MonoBehaviour
     {
         Time.timeScale = timeScaleAtStart;
         SceneManager.LoadScene("Deity");
+    }
+
+    public void SettingsToggle(bool visibility)
+    {
+        inSettingsMenu         = visibility;
+        settingsCanvas.enabled = visibility;
+        mainCanvas.enabled     = !visibility;
+
+        if (!visibility) SetSettings();
+    }
+    void SetSettings()
+    {
+        
     }
 }
