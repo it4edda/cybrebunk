459f584999464a96d1fd43b5b671d1ec2d7a3eed
Edmalm <nilswilliam2005@gmail.com>
2023-11-23 13:40:24 +0100

    xcfvghjklö

--- CybreBunk/Assets/Scripts/Z Unrelated/NilsCounter.cs
+++ CybreBunk/Assets/Scripts/Z Unrelated/NilsCounter.cs
@@ -1,25 +1,7 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NilsCounter
+public static class NilsCounter
 {
-    /*public string SplitStrings(string mainString, string keyWord, string number)
-    {
-        string a1 = "William";
-        string a2 = "Edmalm";
-        if (mainString.Contains(keyWord))
-        {
-            string[] parts = mainString.Split(new[] { keyWord }, 2, System.StringSplitOptions.None);
-            a1 = parts[0].Trim();
-            a2 = parts[1].Trim();
-        }
-        else return mainString;
-        string result = a1 + " " + number + " " + a2;
-        return result;
-    }*/
-    public string SplitStrings(string mainString, NilsEnum keyWord, string number)
+    public static string SplitStrings(string mainString, NilsEnum keyWord, string number)
     {
         string a1   = "William";
         string a2   = "Edmalm";
@@ -34,9 +16,5 @@ public class NilsCounter
         string result = a1 + " " + number + " " + a2;
         return result;
     }
-    [Serializable]
-    public enum NilsEnum
-    {
-        @damage, block, heal
-    }
+    [Serializable] public enum NilsEnum { @damage, @block, @heal }
 }
