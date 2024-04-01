9580dec0c6ba5aba64d31ab92f06307eefd43690
William Edmalm <nilswilliam2005@gmail.com>
2024-04-01 20:51:56 +0200

    adadad

--- CybreBunk/Assets/Scripts/Game/Player/Abilities/Ability.cs
+++ CybreBunk/Assets/Scripts/Game/Player/Abilities/Ability.cs
@@ -1,40 +1,40 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
-using UnityEditor;
 using UnityEngine;
 
 public class Ability : MonoBehaviour
 {
     [Header("Here's your header Alex")]
-    [SerializeField] ChosenAbility ability1;
-    [SerializeField] ChosenAbility ability2;
-    PlayerMovement                 playerMovement;
+    public ChosenAbility ability1;
+    public ChosenAbility ability2;
+    PlayerMovement       playerMovement;
+    
+    [Header("Dark Arts"), SerializeField] DarkArtsVariables  darkArtsVariables;
+    [Header("AoeAttack"), SerializeField] AoeAttackVariables aoeAttackVariables;
 
-    [Header("Vinushka")]
-    [SerializeField] DarkArtsVariables darkArtsVariables;
-    enum ChosenAbility
+    public enum ChosenAbility
     {
         DarkArts,
-        B,
+        AoeAttack,
         C
     }
 
     void Start()
     {
-        playerMovement = GetComponent<PlayerMovement>();
+        playerMovement                  = GetComponent<PlayerMovement>();
+        darkArtsVariables.canUseAbility = true;
     }
 
-    void Initiate()
+    public void InitiateAbility(ChosenAbility chosen)
     {
-        switch (ability1)
+        switch (chosen)
         {
             case ChosenAbility.DarkArts:
                 StartCoroutine(DarkArts());
                 break;
 
-            case ChosenAbility.B:
+            case ChosenAbility.AoeAttack:
+                
                 break;
 
             case ChosenAbility.C:
@@ -47,34 +47,45 @@ public class Ability : MonoBehaviour
 
 #region Vinushka
 #region  variables
+    
     [Serializable]
-    struct Vinushka
-    {
-        DarkArtsVariables darkArtsVariables;
-    }
     struct DarkArtsVariables
     {
+        public bool         canUseAbility;
         public float        timeActive;
+        public float        movementBoost; //ADDITIVE
         public LineRenderer lineRenderer;
+    }
+    [Serializable]
+    struct AoeAttackVariables
+    {
+        public float      timeActive;
+        public Collider2D damageCollider;
     }
 #endregion
     IEnumerator DarkArts() //name of the item in isaac, im not THAT edgy
     {
-        DarkArtsVariables darkArtsLineVariables;
+        Debug.Log("HAHA DID DARK ARTS");
+
+        if (!darkArtsVariables.canUseAbility) yield break;
+
+        playerMovement.MoveSpeed += Vector2.one * darkArtsVariables.movementBoost;
         
-        
-        //higher ms
         //become gray
         //on collision ; stun enemies, stun projectiles
 
         yield return new WaitForSeconds(1); //make variable for time
         
+        playerMovement.MoveSpeed -= Vector2.one * darkArtsVariables.movementBoost;
         //after delay ; release and damage enemies, delete projectiles
         //become normal color (racist)
-        
-        
+
+
         //LINE RENDERER
     }
-    //ADD SCRIPT "PLAYER INTERACTION"
+    IEnumerator AoeAttack()
+    {
+        yield return new WaitForSeconds(1);
+    }
 #endregion
 }
--- CybreBunk/Assets/Scripts/Game/Player/PlayerInput.cs
+++ CybreBunk/Assets/Scripts/Game/Player/PlayerInput.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    PauseMenu                  pauseMenu;
-    PlayerAttack               playerAttack;
+    PauseMenu    pauseMenu;
+    PlayerAttack playerAttack;
+    Ability      ability;
     void Start()
     {
         pauseMenu    = FindObjectOfType<PauseMenu>();
         playerAttack = FindObjectOfType<PlayerAttack>();
+        ability      = FindObjectOfType<Ability>();
+
     }
     void Update()
     {
@@ -18,6 +21,9 @@ public class PlayerInput : MonoBehaviour
         
         if (pauseMenu.IsPaused) return;
         if (Input.GetKey(KeyCode.Mouse0)) playerAttack.Attack();
+        if (Input.GetKey(KeyCode.Mouse1)) ability.InitiateAbility(ability.ability1);
+        if (Input.GetKey(KeyCode.Mouse2)) ability.InitiateAbility(ability.ability2);
+        
         //movement input is checked in "PlayerMovement"
         //interact action is checked in "Interaction"'s active function
     }
