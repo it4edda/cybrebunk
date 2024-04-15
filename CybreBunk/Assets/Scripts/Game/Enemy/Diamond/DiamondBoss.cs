f9abe0bed6398d20e259cf3159029bdfb48465b0
William Edmalm <nilswilliam2005@gmail.com>
2024-04-15 21:28:48 +0200

    daad

---
+++ CybreBunk/Assets/Scripts/Game/Enemy/Diamond/DiamondBoss.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DiamondBoss : EnemyBehaviour
+{
+    [SerializeField] CustomBulletShooter shooter;
+    [SerializeField] GameObject[]        enemiesToSpawn;
+    [SerializeField] Vector3[]           spawnPositions;
+    [SerializeField] float               breakTime;
+    [SerializeField] float               timeBetweenShove;
+    [SerializeField] int                 timesToShove;
+    int                                  shoving;
+    bool                                 shooting;
+    
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+        shoving = timesToShove;
+    }
+    
+    IEnumerator SpawnWaves()
+    {
+        shooting = false;
+
+        foreach (var i in enemiesToSpawn)
+        {
+            Instantiate(i, transform.position + spawnPositions[Random.Range(0, spawnPositions.Length)] ,Quaternion.identity);
+            yield return new WaitForSeconds(0.2f);
+        }
+        
+        yield return new WaitForSeconds(timeBetweenShove);
+
+        shoving--;
+
+        if (shoving <= 0)
+        {
+            shoving = timesToShove;
+            shooter.ChooseNewRoutine();
+        }
+        yield return new WaitForSeconds(breakTime);
+        StartCoroutine(SpawnWaves());
+    }
+    protected override IEnumerator Knockback(Vector2 dir) { yield break; }
+    protected override void Movement() { return; }
+
+    void OnDrawGizmosSelected()
+    {
+        foreach (var i in spawnPositions)
+        {
+            Gizmos.DrawWireSphere(i+ transform.position, 0.02f);
+        }
+    }
+}
